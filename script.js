86d928c7b8233191b9ee23a3e9268db723315ff2
yoramtaieb <taieb.yoram@gmail.com>
2020-03-16 17:12:47 +0100

    changement phrase projet

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 $('body').toggleClass('day-background');
 
-$('#cb1').on('click', function () { $('body').toggleClass('day-background'); });
+$('#cb1').on('click', function() { $('body').toggleClass('day-background'); });
